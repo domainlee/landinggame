a85fa79d2591aa836894381b16057039da21018b
tungpt <tungpt9x2@gmail.com>
2021-11-19 18:03:16 +0700

    update code

--- assets/js/s.js
+++ assets/js/s.js
@@ -9,8 +9,10 @@ $(function () {
   // });
 
   var $isAnimatedGamePlay = $(".gameplay .is-animated");
+  var $isAnimatedGem = $(".gem-item .is-animated");
   var $isAnimatedRoadmap = $(".roadmap .is-animated");
   var $isAnimatedPartner = $(".partner .is-animated");
+  var $isAnimatedTokenomics = $(".tokenomics .is-animated");
 
   // fullpage customization
   $("#fullpage").fullpage({
@@ -29,6 +31,7 @@ $(function () {
       "gem",
       "team",
       "roadmap",
+      "tokenomics",
       "partner",
       "footer",
     ],
@@ -56,13 +59,24 @@ $(function () {
         $header_top.addClass("header-transparent");
       }
 
-      if ((index == 1 && nextIndex == 2) || (index == 3 && nextIndex == 2)) {
+      if ((index == 2 && nextIndex == 3) || (index == 4 && nextIndex == 3)) {
         $isAnimatedGamePlay.addClass("animated fadeInUpBig");
       } else {
         $isAnimatedGamePlay.removeClass("animated fadeInUpBig");
       }
 
-      if ((index == 5 && nextIndex == 6) || (index == 7 && nextIndex == 6)) {
+      if ((index == 4 && nextIndex == 5) || (index == 6 && nextIndex == 5)) {
+        $isAnimatedGem.addClass("animated fadeInUp");
+        $isAnimatedGem.eq(0).css("animation-delay", ".2s");
+        $isAnimatedGem.eq(1).css("animation-delay", ".4s");
+        $isAnimatedGem.eq(2).css("animation-delay", ".6s");
+        $isAnimatedGem.eq(3).css("animation-delay", ".8s");
+        $isAnimatedGem.eq(4).css("animation-delay", "1s");
+      } else {
+        $isAnimatedGem.removeClass("animated fadeInUp");
+      }
+
+      if ((index == 6 && nextIndex == 7) || (index == 8 && nextIndex == 7)) {
         $isAnimatedRoadmap.addClass("animated fadeIn");
         $isAnimatedRoadmap.eq(0).css("animation-delay", ".3s");
         $isAnimatedRoadmap.eq(1).css("animation-delay", ".6s");
@@ -74,12 +88,22 @@ $(function () {
         $isAnimatedRoadmap.removeClass("animated fadeIn");
       }
 
-      if ((index == 6 && nextIndex == 7) || (index == 7 && nextIndex == 6)) {
-        $isAnimatedPartner.addClass("animated fadeIn");
-        $isAnimatedPartner.eq(0).css("animation-delay", ".3s");
-        $isAnimatedPartner.eq(1).css("animation-delay", ".6s");
+      if ((index == 7 && nextIndex == 8) || (index == 9 && nextIndex == 8)) {
+        $isAnimatedTokenomics.addClass("animated fadeIn");
+        $isAnimatedTokenomics.eq(0).css("animation-delay", ".2s");
+        $isAnimatedTokenomics.eq(1).css("animation-delay", ".4s");
+        $isAnimatedTokenomics.eq(2).css("animation-delay", ".6s");
+        $isAnimatedTokenomics.eq(3).css("animation-delay", ".8s");
+        $isAnimatedTokenomics.eq(4).css("animation-delay", "1s");
+        $isAnimatedTokenomics.eq(5).css("animation-delay", "1.2s");
+        $isAnimatedTokenomics.eq(6).css("animation-delay", "1.4s");
+        $isAnimatedTokenomics.eq(7).css("animation-delay", "1.6s");
+        $isAnimatedTokenomics.eq(8).css("animation-delay", "1.8s");
+        $isAnimatedTokenomics.eq(9).css("animation-delay", "2s");
+        $isAnimatedTokenomics.eq(10).css("animation-delay", "2.2s");
+        $isAnimatedTokenomics.eq(11).css("animation-delay", "2.4s");
       } else {
-        $isAnimatedPartner.removeClass("animated fadeIn");
+        $isAnimatedTokenomics.removeClass("animated fadeIn");
       }
     },
 
